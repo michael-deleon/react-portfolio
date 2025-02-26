c0e2ba7a817118ba7e8f14265cdb4259c08c3087
michael-deleon <deleonjmike@gmail.com>
2025-02-26 17:20:22 -0500

    Implemented Analytics Feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // Import FontAwesome sty
 config.autoAddCss = true; 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
